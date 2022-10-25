a9a82585103cd36f2ca7bf9d9a4a6f97536289ea
Adrián Blanco Fernández <adrianblfe@gmail.com>
2022-10-25 23:39:21 +0200

    feat(pages/results/index.vue): añadida página de resultados

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,45 @@
 <template>
-  <v-row>
-    <v-col cols="6" offset="3">
-      <p id="typer"></p>
-    </v-col>
-    
-    <v-col v-if="showInitGameButton" cols="6" offset="3" class="d-flex justify-end">
-      <GenericLink :to="'/quiz'">Comenzar</GenericLink>
-    </v-col>
-  </v-row>
+	<v-row>
+		<v-col cols="6" offset="3">
+			<p id="typer"></p>
+		</v-col>
+	
+		<v-col v-if="showInitGameButton" cols="6" offset="3" class="d-flex justify-end">
+			<GenericLink :to="'/quiz'">Comenzar</GenericLink>
+		</v-col>
+ 	</v-row>
 </template>
 
 <script>
 import typer from 'typer-js';
 
 export default {
-  name: 'IndexPage',
-  component: {
-    GenericLink: () => import('@/components/genericLink.vue')
-  },
-  data() {
-    return {
-      showInitGameButton: false
-    };
-  },
-  mounted() {
-    this.typeIntroduction();
-  },
-  methods: { 
-    typeIntroduction() {
-      const element = document.querySelector('#typer');
-      typer(element, 9) // Añadir 90 de velocidad
-        .line('Hola humano, eres el último de tu especie')
-        .line('está en tus manos la oportunidad')
-        .line('de salvar a la humanidad, para ello,')
-        .line('deberás contestar correctamente a 10 preguntas...')
-        .line('¿Estás preparado?')
-        .end(() => {
-          this.showInitGameButton = true
-        });
-    },
-  }
+  	name: 'IndexPage',
+  	component: {
+		GenericLink: () => import('@/components/genericLink.vue')
+  	},
+	data() {
+		return {
+			showInitGameButton: false
+		};
+	},
+	mounted() {
+		this.typeIntroduction();
+	},
+	methods: { 
+		typeIntroduction() {
+			const element = document.querySelector('#typer');
+			typer(element, 9) // Añadir 90 de velocidad
+				.line('Hola humano, eres el último de tu especie')
+				.line('está en tus manos la oportunidad')
+				.line('de salvar a la humanidad, para ello,')
+				.line('deberás contestar correctamente a 10 preguntas...')
+				.line('¿Estás preparado?')
+				.end(() => {
+					this.showInitGameButton = true
+				});
+		},
+	}
 }
 </script>
 
--- pages/results/index.vue
+++ pages/results/index.vue
@@ -1,14 +1,65 @@
 <template>
     <div class="row">
-        <div class="col-12">
-            <h1>resultados</h1>        
+        <div class="col-12 d-flex justify-center">
+            <p id="results-typer-header"></p>        
+        </div>
+        <div class="col-12 d-flex justify-center">
+            <div id="results-typer"></div>
         </div>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import typer from 'typer-js';
+
 export default {
-    
+    data() {
+        return {};
+    },
+    computed: {
+        ...mapState(['quiz', 'answers'])
+    },
+    mounted() {
+        this.typeResultsHeader();
+    },
+    methods: {
+        typeResultsHeader() {
+            const element = document.querySelector('#results-typer-header');
+            typer(element, 9) // Añadir 90 de velocidad
+                .line('De acuerdo, ya vemos que has finalizado la prueba...')
+                .line('A continuación vamos a proceder a revisar tus respuesta...')
+                .end(() => {
+                    this.initReview();
+                });
+        },
+
+        initReview() {
+            const element = document.querySelector('#results-typer');
+            const typerResults = typer(element, 9); 
+            this.answers.forEach((answer, index) => {
+                typerResults.line(`${ index + 1 }. ${ answer}... ${ this.getResult(answer, index) }`);
+            });
+        },
+
+        getResult( answerUser, index ) {
+            const { answer, type } = this.quiz[index];
+            const correctIcon = '&#10003;';
+            const incorrectIcon = '&#215;';
+
+            if (['select', 'text', 'radiobutton'].includes(type)) {
+                return answer.trim().toLowerCase().includes(answerUser.trim().toLowerCase()) ? correctIcon : incorrectIcon;
+            }
+
+            if (type === 'number') {
+                return answer.toString() === answerUser ? correctIcon : incorrectIcon;
+            }
+
+            if (type === 'checkbox') {
+                return answer.length === 2 && answerUser.every((item) => answer.includes(item)) ? correctIcon : incorrectIcon;
+            }
+        }
+    }
 }
 </script>
 
